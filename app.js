8e78e4b0b0486b843593a123fce25a49c48ac5ba
Pablo Haddad <p.haddad.p@gmail.com>
2016-10-21 17:53:52 -0300

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var todos = require('./routes/todos');
@@ -18,9 +19,67 @@ var  Todos=require('./models/Todos');
 var  BackPoc=require('./models/BackPoc');
 
 
+var debug = require('debug')('flapper-news:server');
+var http = require('http');
+
+
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', "3000");
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+ 
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
  
 
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -73,5 +132,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-console.log("port 3000")
+
 module.exports = app;
